78594024db1ffb836fc7a384fc5612757d135b2f
Ostusuki <21020002@vnu.edu.vn>
2024-01-08 22:15:17 +0700

    Create App.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import { ChannelContainer, ChannelListContainer} from './components';
 
+import './App.css';
+
 const apiKey = 'bydbun9x9q6b';
 
 const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div className="app_wrapper">
+    <div className="app__wrapper">
         <Chat client={client} theme="team light">
             <ChannelListContainer
             
