2a99268fcdcd5c135920bbaac6e79dc6d61b7b34
Ostusuki <21020002@vnu.edu.vn>
2024-01-10 17:39:15 +0700

    Setup SignUp (1 - add Full Name, add User Name)

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,24 +1,47 @@
-import React, { useState} from 'react'
-import Cookie from 'universal-cookie';
-import axios from 'axios';
+import React, { useState } from "react";
+import Cookie from "universal-cookie";
+import axios from "axios";
 
-import signInImage from '../assets/signup.jpg';
+import signInImage from "../assets/signup.jpg";
 
 const Auth = () => {
-    const [isSignUp, setIsSignUp] = useState(false);
+    const [isSignUp, setIsSignUp] = useState(true);
 
-  return (
-    <div className="auth__form-container">
-        <div className="auth__form-container_fields">
-            <div className="auth__form-container_fields-content">
-                <p>
-                    {isSignUp ? 'Sign Up' : 'Sign In'}
-                </p>
+    const handleChange = () => {}
+
+    return (
+        <div className="auth__form-container">
+            <div className="auth__form-container_fields">
+                <div className="auth__form-container_fields-content">
+                    <p>{isSignUp ? "Sign Up" : "Sign In"}</p>
+                    <form onSubmit={() => {}}>
+                        {isSignUp && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="fullName">Full Name</label>
+                                <input
+                                    name="fullName"
+                                    type="text"
+                                    placeholder="Full Name"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="useName">User Name</label>
+                            <input
+                                name="useName"
+                                type="text"
+                                placeholder="User Name"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-
-    </div>
-  )
+    );
 }
 
-export default Auth
+export default Auth;
