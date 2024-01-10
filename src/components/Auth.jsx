945c9e5163c6d374f6d8a98f7803543d544db25e
Ostusuki <21020002@vnu.edu.vn>
2024-01-10 17:50:53 +0700

    Setup Sign Up (2 - add Phone Number, Avatar Url, Password, Confirm Password)

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -7,14 +7,14 @@ import signInImage from "../assets/signup.jpg";
 const Auth = () => {
     const [isSignUp, setIsSignUp] = useState(true);
 
-    const handleChange = () => {}
+    const handleChange = () => { }
 
     return (
         <div className="auth__form-container">
             <div className="auth__form-container_fields">
                 <div className="auth__form-container_fields-content">
                     <p>{isSignUp ? "Sign Up" : "Sign In"}</p>
-                    <form onSubmit={() => {}}>
+                    <form onSubmit={() => { }}>
                         {isSignUp && (
                             <div className="auth__form-container_fields-content_input">
                                 <label htmlFor="fullName">Full Name</label>
@@ -37,6 +37,52 @@ const Auth = () => {
                                 required
                             />
                         </div>
+                        {isSignUp && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="phoneNumber">Phone Number</label>
+                                <input
+                                    name="phoneNumber"
+                                    type="text"
+                                    placeholder="Phone Number"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        {isSignUp && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="fullName">Avatar Url</label>
+                                <input
+                                    name="avatarURL"
+                                    type="text"
+                                    placeholder="Avatar URL"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {isSignUp && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                <input
+                                    name="confirmPassword"
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
                     </form>
                 </div>
             </div>
