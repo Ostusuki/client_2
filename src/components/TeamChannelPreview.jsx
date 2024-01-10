07c1738c77006eedc97be503a3c68c92b0364976
Ostusuki <21020002@vnu.edu.vn>
2024-01-10 17:01:26 +0700

    Setting for messaging (no database so return error) - fix 1

--- src/components/TeamChannelPreview.jsx
+++ src/components/TeamChannelPreview.jsx
@@ -27,8 +27,8 @@ const TeamChannelPreview = ({channel, type}) => {
     return (
         <div className={
             channel?.id === activeChannel?.id
-                ? 'channel-preview__wrapper__selection'  
-                ? 'channel-preview__wrapper' 
+                ? 'channel-preview__wrapper__selected'  
+                : 'channel-preview__wrapper' 
         }
         onClick={() => {
             console.log(channel);
